62b8a2fb030766642168102282fc5903660d7591
zycgod-opop <2083172064@qq.com>
2026-09-26 23:57:23 +0800

    凌波丽版缓存v2：更新图标与图片

--- rei/sw.js
+++ rei/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'habit-checkin-rei-v1';
+const CACHE = 'habit-checkin-rei-v2';
 const ASSETS = ['./', './index.html', './assets/rei-emblem.svg', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -29,6 +29,7 @@ self.addEventListener('fetch', (e) => {
     }))
   );
 });
+
 
 
 

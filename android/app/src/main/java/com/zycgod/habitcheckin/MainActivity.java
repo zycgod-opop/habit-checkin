51f3f42315ca818dec6f072ea3ca2234cda5ab68
zycgod-opop <2083172064@qq.com>
2026-09-26 23:11:21 +0800

    安卓快船主版

--- android/app/src/main/java/com/zycgod/habitcheckin/MainActivity.java
+++ android/app/src/main/java/com/zycgod/habitcheckin/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.ProgressBar;
 import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
-    private static final String HOME = "https://zycgod-opop.github.io/habit-checkin/rei/";
+    private static final String HOME = "https://zycgod-opop.github.io/habit-checkin/";
     private WebView web;
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -70,4 +70,5 @@ public class MainActivity extends Activity {
         else { super.onBackPressed(); }
     }
 }
+
 

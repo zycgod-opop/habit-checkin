e273ab5080d8da2ea0d512df86d122605a01a5e2
zycgod-opop <2083172064@qq.com>
2026-09-26 23:07:47 +0800

    双版本：rei/sw.js

---
+++ rei/sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'habit-checkin-rei-v1';
+const ASSETS = ['./', './index.html', './assets/rei-emblem.svg', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;
+  if (req.mode === 'navigate') {
+    e.respondWith(
+      fetch(req).then((res) => {
+        const cp = res.clone();
+        caches.open(CACHE).then((c) => c.put('./index.html', cp));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(req).then((hit) => hit || fetch(req).then((res) => {
+      const cp = res.clone();
+      caches.open(CACHE).then((c) => c.put(req, cp));
+      return res;
+    }))
+  );
+});
+
+
+
